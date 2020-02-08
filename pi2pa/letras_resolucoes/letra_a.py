9d56582dfb7cededd513c33d7b7ef4154be71f44
Pedro <pedrohceravolo@gmail.com>
2020-02-07 20:24:37 -0400

    feat: improve letra a

--- pi2pa/letras_resolucoes/letra_a.py
+++ pi2pa/letras_resolucoes/letra_a.py
@@ -12,19 +12,35 @@ product = 1561893684
 
 sc._jsc.hadoopConfiguration().set("textinputformat.record.delimiter", "\n\n")
 
-def format_review(r):
-    helpful = re.search(r'helpful:\s+(\d+)', r).group(1)
-    rating = re.search(r'rating:\s+(\d+)', r).group(1)
-    return (r, (int(helpful), int(rating)))
+def customPrint(arr):
+    for el in arr:
+        print('review: {} | com rating {} e helpful {}'.format(el[0], el[1][0], el[1][1]))
+
+def getReviews(stripped):
+  allReviews = []
+  for i in range(len(stripped)):
+    if stripped[i].startswith('reviews'):
+      for j in range(i+1, len(stripped)):
+        allReviews.append(stripped[j])
+      return allReviews
+
+def format(r):
+    return (r.split('rating')[0], (int(r.split('rating:')[1][0]), int(r.split('rating:')[1][0])))        
 
 selected = file.filter(
     lambda item: re.search(str(f'ASIN:[ ]*{product}'), item))
-reviews = selected.flatMap(
-    lambda item: re.findall(r'\d+-\d+-\d+\s+cutomer:[\s\w\d]+rating:[\s\d]+votes:[\s\d]+helpful:\s+\d+', item))
-reviews = reviews.map(format_review)
+
+asi = selected.map(lambda s: s.rstrip().split('\n'))
+stripped = asi.map(lambda s: [elem.replace(" ","") for elem in s])
+review = stripped.flatMap(getReviews)
+reviews = review.map(format)
+
+print('\n5 comentários mais úteis e com maior avaliação: ')
 max_reviews = reviews.sortBy(lambda r: r[1][1], ascending=False)
 sorted_max_reviews = max_reviews.takeOrdered(5, lambda r: -r[1][0])
-print(sorted_max_reviews)
+customPrint(sorted_max_reviews)
+
+print('\n5 comentários mais úteis com menor avaliação: ')
 min_reviews = reviews.sortBy(lambda r: r[1][1])
 sorted_min_reviews = min_reviews.takeOrdered(5, lambda r: -r[1][0])
-print(sorted_min_reviews)
+customPrint(sorted_min_reviews)
