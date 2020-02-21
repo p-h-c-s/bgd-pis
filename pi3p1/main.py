990cabfe9caf06254de8253f32434ae9cc685702
Pedro <pedrohceravolo@gmail.com>
2020-02-21 19:37:29 -0400

    feat: tidy up everything.

--- pi3p1/main.py
+++ pi3p1/main.py
@@ -5,7 +5,6 @@ from pyspark.sql.functions import *
 from pyspark.ml.linalg import Vectors
 from pyspark.sql.types import *
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, CountVectorizer, Word2Vec, MinHashLSH
-
 from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from operator import itemgetter
@@ -20,7 +19,8 @@ spark.sparkContext.setLogLevel("OFF")
 proc_data = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
 
 TOTAL_AMOUNT = len(proc_data.target)
-# TEST_AMOUNT = TOTAL_AMOUNT/3
+
+# amount of test values that are actually used. Low value for quick testing!!!
 TEST_AMOUNT = 10
 
 # k parameter for A-NN
@@ -32,34 +32,30 @@ NUM_HASH_TABLES = 5
 raw_groups = list(zip(proc_data.data, proc_data.target.tolist()))
 raw_groups[:] = [x for x in raw_groups if (len(x[0].replace('\n', '')) > 0)]
 
+# Pipeline function to perform all transformations
 def vectorizeDF(raw):
   raw = spark.createDataFrame(raw_groups, schema=['data', 'target'])
   raw = raw.withColumn('id', monotonically_increasing_id())
 
   tokenizer = Tokenizer(inputCol='data', outputCol='tokens')
-  # tok_data = tokenizer.transform(raw)
 
   swremover = StopWordsRemover(inputCol='tokens', outputCol='words')
-  # rm_data = swremover.transform(tok_data)
 
-  cv = CountVectorizer(inputCol='words', outputCol='features', vocabSize=1000)
-  # cvmodel = cv.fit(rm_data)
+  cv = CountVectorizer(inputCol='words', outputCol='features', vocabSize=100)
 
-  pipeline = Pipeline(stages=[tokenizer,swremover,cv])
+  mh = MinHashLSH(inputCol='features', outputCol='hashes', numHashTables=NUM_HASH_TABLES, seed = 5123)
+
+  pipeline = Pipeline(stages=[tokenizer,swremover,cv, mh])
   feat_data = pipeline.fit(dataset=raw).transform(raw)
   checkZero = udf(lambda V: V.numNonzeros() > 0, BooleanType())
 
   feat_data = feat_data.filter(checkZero(col('features')))
   return feat_data
 
-# create dataframe from raw data
+# apply entire pipeline to raw_data
 train = vectorizeDF(proc_data)
 
-# Create LSH model
-mh = MinHashLSH(inputCol='features', outputCol='hashes', numHashTables=NUM_HASH_TABLES, seed = 5123)
-model = mh.fit(train)
-train = model.transform(train)
-
+# extract hashes from denseVectors
 extractExploded = udf(lambda l: float(l[0]), FloatType())
 train = train.select('id', 'features', 'target', explode('hashes').alias('hashes'))
 train = train.withColumn('extracted', extractExploded(col('hashes')))
@@ -76,10 +72,10 @@ def extractValues(test_value):
   values['extracted'] = test_value['extracted']
   return values
 
+# take only a few values for quick testing.
 test_values = teste.take(TEST_AMOUNT)
 
-# ver se tem pelo menos um hash igual. se tiver, usar a keyDist
-# vect2 por exemplo e o vetor de teste
+# Jaccard distance for two sparse vectors
 def keyDist(vect1, vect2):
   set1 = set(vect1)
   set2 = set(vect2)
@@ -87,22 +83,27 @@ def keyDist(vect1, vect2):
   union = float(len(set1) + len(set2) - intersectSize)
   return 1 - intersectSize/union
 
+# Helper function for jaccard distance
 def distAux(keyFeat):
   return udf(lambda l: keyDist(l, keyFeat), DoubleType())
 
-
+""" Main A-NN function.
+  The basic idea is using the hashes from minhashLSH to find candidate pairs. 
+  Then we measure the distance between those candidate pairs with jaccard distance. 
+  The 'k' closest values then 'vote' to classify the test entry.
+"""
 def A_NN(test_values, train_dataframe):
   predictionsAndLabels = []
   for value in test_values:
     value = extractValues(value)
 
-    # assumindo que o spark sabe comparar floats de maneira decente
     hashEncounters = train_dataframe.where('extracted == {}'.format(value['extracted']))
-
     print('A-NN para a entrada de teste de id: {} com target: {}'.format(value['id'], value['target']))
     dists = hashEncounters.withColumn('distCol', distAux(value['features'])(col('features')))
     topK = dists.orderBy(col('distCol').desc()).limit(K_VALUE)
     predictedList = topK.select('target').collect()
+
+    # vote for the most common category in k categories
     votes = {}
     if(len(predictedList) > 0):
       for target in predictedList[0]:
@@ -114,11 +115,14 @@ def A_NN(test_values, train_dataframe):
       selectedTarget = builtins.max(votes.items(), key = itemgetter(1))[0]
       predictionsAndLabels.append((selectedTarget, value['target']))
   metricsDf = spark.createDataFrame(predictionsAndLabels,['pred','label'])
-  metricsDf = metricsDf.withColumn('newPred', metricsDf.pred.cast(DoubleType()))
-  metrics = MulticlassClassificationEvaluator(predictionCol='newPred', labelCol='label')
+  metricsDf = metricsDf.withColumn('prediction', metricsDf.pred.cast(DoubleType()))
+  metrics = MulticlassClassificationEvaluator(predictionCol='prediction', labelCol='label')
+  print('\npreview do dataframe de resultados: ')
+  metricsDf.select('label', 'prediction').show()
   print('precision: ', metrics.evaluate(metricsDf, {metrics.metricName :'weightedPrecision'}))
   print('recall: ', metrics.evaluate(metricsDf, {metrics.metricName :'weightedRecall'}))
   print('F1: ', metrics.evaluate(metricsDf,{metrics.metricName :'f1'}))
 
+  print('\nCategorias estão em forma numérica para tratamento interno')
 
 A_NN(test_values,train)
